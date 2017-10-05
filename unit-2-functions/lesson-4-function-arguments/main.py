8c8de7dec1dce62df75f739e758db75cc8f6ab90
Santiago Basulto <santiago.basulto@gmail.com>
2017-10-05 17:39:59 -0300

    Update main.py

--- unit-2-functions/lesson-4-function-arguments/main.py
+++ unit-2-functions/lesson-4-function-arguments/main.py
@@ -1,13 +1,16 @@
 def multiply_by_two(x):
     pass
 
-multiply_by_two(5)
-multiply_by_two(10)
+# --- Testing ---
+# Use the code below for your own testing
+# by clicking the "Run Code" button.
+# After you're done, remove the lines below and "Submit Solution".
 
-# two_times_two = multiply_by_two(2)
-# three_times_two = multiply_by_two(3)
-# five_times_two = multiply_by_two(5)
-#
-# print("2x2 = {}".format(two_times_two))
-# print("3x2 = {}".format(three_times_two))
-# print("5x2 = {}".format(five_times_two))
+two_times_two = multiply_by_two(2)
+three_times_two = multiply_by_two(3)
+five_times_two = multiply_by_two(5)
+
+print("2x2 = {}".format(two_times_two))
+print("3x2 = {}".format(three_times_two))
+print("5x2 = {}".format(five_times_two))
+# --- Testing ---
